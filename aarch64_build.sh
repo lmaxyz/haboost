b79f7d68f1050f704b2eae0407948397ad7e74b5
Maxim Leyman <maximleyman@yandex.ru>
2025-07-01 02:49:42 +0700

    fix rpm package version

--- aarch64_build.sh
+++ aarch64_build.sh
@@ -12,10 +12,11 @@ CURRENT_DIR="$(pwd)";
 TARGET=aarch64-unknown-linux-gnu
 
 aurora_psdk="$PSDK_DIR/sdk-chroot"
+PKG_VERSION="$(cargo pkgid | cut -d @ -f 2)"
 
 mkdir -p RPMS/
 
 cross build --release --target $TARGET
 cargo generate-rpm -a aarch64 --target $TARGET -o RPMS/
 
-$aurora_psdk rpmsign-external sign -k $PSDK_DIR/../../certs/lmaxyz_key.pem -c $PSDK_DIR/../../certs/lmaxyz_cert.pem $CURRENT_DIR/RPMS/com.lmaxyz.Haboost-0.1.0-1.aarch64.rpm
+$aurora_psdk rpmsign-external sign -k $PSDK_DIR/../../certs/lmaxyz_key.pem -c $PSDK_DIR/../../certs/lmaxyz_cert.pem "$CURRENT_DIR/RPMS/com.lmaxyz.Haboost-$PKG_VERSION-1.aarch64.rpm"
