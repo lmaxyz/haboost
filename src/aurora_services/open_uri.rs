e6a699160e14d3a96352dd2b2e8fd3a4f7bc30d1
Maksim Leiman <maximleyman@yandex.ru>
2025-12-29 01:33:49 +0300

    add error logging

--- src/aurora_services/open_uri.rs
+++ src/aurora_services/open_uri.rs
@@ -1,17 +1,31 @@
-use dbus::blocking::Connection;
 use dbus::arg::Variant;
+use dbus::blocking::Connection;
 use std::{collections::HashMap, time::Duration};
-
 
 pub fn open_uri(uri: &str) -> Result<(), Box<dyn std::error::Error>> {
     let conn = Connection::new_session()?;
-    let proxy = conn.with_proxy("ru.omp.RuntimeManager", "/ru/omp/RuntimeManager/Intents1", Duration::from_millis(500));
+    let proxy = conn.with_proxy(
+        "ru.omp.RuntimeManager",
+        "/ru/omp/RuntimeManager/Intents1",
+        Duration::from_millis(500),
+    );
 
     let hints: HashMap<String, Variant<String>> = HashMap::new();
     let mut data: HashMap<String, Variant<String>> = HashMap::new();
     data.insert("uri".to_string(), Variant(uri.to_string()));
 
-    let (_,): (String,) = proxy.method_call("ru.omp.RuntimeManager.Intents1", "InvokeIntent", ("OpenURI", hints, data)).unwrap_or((String::default(),));
+    match proxy.method_call(
+        "ru.omp.RuntimeManager.Intents1",
+        "InvokeIntent",
+        ("OpenURI", hints, data),
+    ) {
+        Ok(res) => {
+            let _: (String,) = res;
+        }
+        Err(e) => {
+            log::error!("Error with URI opening: {}", e)
+        }
+    }
 
     Ok(())
 }
